57a2072551138ae1ec74c20640972c0e6e0c8932
georges tchianga <georgestcm@gmail.com>
2019-10-24 05:14:28 -0400

    routing added for contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { LoginComponent } from './login/login.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { TermsComponent } from './terms/terms.component';
+import { ContactComponent} from './contact/contact.component'
 const routes: Routes = [
   { path: '', component:HomeComponent, pathMatch: 'full' },
   { path: 'login', component:LoginComponent , pathMatch:'full' },
   { path: 'reset', component:PasswordResetComponent , pathMatch:'full' },
+  { path: 'contact', component:ContactComponent , pathMatch:'full' },
   { path: 'policy/privacy', component:PrivacyComponent , pathMatch:'full' },
   { path: 'policy/terms', component:TermsComponent , pathMatch:'full' },
   { path: '**', component:PagenotfoundComponent , pathMatch:'full' },
